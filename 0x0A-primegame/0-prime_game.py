85b5fb805acbf311f9957b1bf69fd5aed7a31c2d
Osama Abd El Naby <UsamaMahmoud2002@gmail.com>
2024-12-13 01:50:56 +0000

    d

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 
 def is_winner(x, nums):
-    """
-    Determines the winner of 'x' rounds of the Prime Game, given a list 'nums'.
-
-    Args:
-        x: Number of rounds (integer, 1 to 10000).
-        nums: List of consecutive integers (1 to n, n <= 10000).
-
-    Returns:
-        String: Name of the winner ("Maria", "Ben"), or "None" if winner cannot be determined.
-    """
-
     maria_wins, ben_wins = 0, 0
 
     for _ in range(x):
         remaining_nums = nums.copy()
-        current_player = "Maria"  # Maria always starts
+        current_player = "Maria"  # Maria starts first
 
         while remaining_nums:
             prime = find_first_prime(remaining_nums)
@@ -36,35 +25,13 @@ def is_winner(x, nums):
     else:
         return "None"
 
-
 def find_first_prime(nums):
-    """
-    Finds the first prime number in the given list 'nums'.
-
-    Args:
-        nums: List of integers.
-
-    Returns:
-        Integer: The first prime number found, or None if no prime is found.
-    """
-
     for num in nums:
         if is_prime(num):
             return num
     return None
 
-
 def is_prime(num):
-    """
-    Checks if a number 'num' is prime.
-
-    Args:
-        num: Integer.
-
-    Returns:
-        Boolean: True if 'num' is prime, False otherwise.
-    """
-
     if num <= 1:
         return False
     for i in range(2, int(num**0.5) + 1):
@@ -72,19 +39,17 @@ def is_prime(num):
             return False
     return True
 
-
 def remove_multiples(nums, prime):
-    """
-    Removes the prime number 'prime' and its multiples from the list 'nums'.
-
-    Args:
-        nums: List of integers (modified in-place).
-        prime: Integer, the prime number to remove.
-    """
-
     i = 0
     while i < len(nums):
         if nums[i] % prime == 0:
             del nums[i]
         else:
             i += 1
+
+# Example usage
+x = 5
+nums = [2, 5, 1, 4, 3]
+winner = isWinner(x, nums)
+print("Winner:", winner)
+
